3bc39087b7dd5971577d39713f96195664513cc4
Kogaroshi <vigervalentin@gmail.com>
2024-01-29 10:44:09 -0500

    Set License

--- contracts/HolyPalPower.sol
+++ contracts/HolyPalPower.sol
@@ -6,7 +6,7 @@
 //╚═╝     ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═══╝
  
 
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.20;
 
 import {IHolyPaladinToken} from "./interfaces/IHolyPaladinToken.sol";
--- contracts/LootReserve.sol
+++ contracts/LootReserve.sol
@@ -6,7 +6,7 @@
 //╚═╝     ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═══╝
  
 
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.20;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
